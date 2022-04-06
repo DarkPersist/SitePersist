425b62d135e0a9c0c9578a1184611933591cfad3
Sebastián López Osorno <100486485+SebastianLopezOsorno-SENA@users.noreply.github.com>
2022-04-06 13:20:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,25 +4,10 @@
     <meta charset="UTF-8">
     <title>SitePersist</title>
     <style>
-        #demotext {
-            color: #FFFFFF;
-            background: #232323;
-            text-shadow: 0 0 5px #FFF, 0 0 10px #FFF, 0 0 15px #FFF, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18;
-            color: #FFFFFF;
-            background: #232323;
-            display:flex;
-            justify-content: center;
-            align-items: center;
-            font-size:1000;
+        body{
+            background:rgb(random(255),random(255),random(255));
         }
-        
-        * { 
-            background: #18264F; 
-        }
-        
     </style>
 </head>
 <body>
-<div id="demotext">SitePersist</div>
-</body>
 </html>
