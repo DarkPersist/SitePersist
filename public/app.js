4e26e7499e30ac1b49fdebce37ffb539d6de3f33
Sebastián López Osorno <slopez7185@misena.edu.co>
2022-06-18 22:23:07 -0500

    m

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
 const http = require('http');
 
-http.createServer(function (req, res){
+const app=http.createServer(function (req, res){
+    res.writeHead(200,{'Content-type': 'text/html'});
     res.write('<h1>hola mundo</h1>');
+    res.end();
 });
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT);
