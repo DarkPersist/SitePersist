6526753f5ccfe56534d4be5c04d3bf7d7c8a7d0a
Sebastián López Osorno <slopez7185@misena.edu.co>
2022-10-10 21:11:51 -0500

    main

---
+++ server/connection/conexion.php
@@ -0,0 +1,21 @@
+<?php
+	/* Conexion en xampp*/
+	$server = 'localhost';
+	$username = 'root';
+	$password = '';
+	$database = 'sitepersist';
+
+	/* Conexion en remotemysql.com
+	$server = 'remotemysql.com';
+	$username = 'W80NGrZVyT';
+	$password = 'VU7ZJIPVe8';
+	$database = 'W80NGrZVyT';
+	*/
+
+	try {
+	    $conx= mysqli_connect($server,$username,$password);
+	    mysqli_select_db($conx, $database);
+	} catch (mysqli_sql_exception $fail) {
+	    die('La Conexión ha sido Fallida: ' . $fail->getMessage());
+	}
+?>.
