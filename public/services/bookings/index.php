7f14342c39be5e8a0c8f0313c40d424c5c2ec307
Sebastián López Osorno <slopez7185@misena.edu.co>
2022-10-14 00:09:09 -0500

    main

--- public/services/bookings/index.php
+++ public/services/bookings/index.php
@@ -2,9 +2,90 @@
 <html lang="en">
 <head>
     <?php include "../../resources/assets/meta.html" ?>
+    <style>
+        .banner {
+            background: rgb(141, 4, 133);
+            background: #111111;
+            padding: 200px 100px 100px;
+            min-height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .banner .content {
+            max-width: 700px;
+        }
+
+        .banner .content h2 {
+            font-size: 3em;
+            color: #fff;
+            margin-bottom: 10px;
+        }
+
+        .banner .content p {
+            font-size: 1em;
+            color: #fff;
+            margin: 10px;
+        }
+
+        .banner .content a {
+            display: inline-block;
+            background: #fff;
+            color: #000;
+            padding: 8px 20px;
+            text-decoration: none;
+            font-weight: 600;
+            margin-top: 20px;
+        }
+
+        .banner .image {
+            max-width: 400px;
+            margin-left: 30px;
+        }
+
+        @media(max-width:991px) {
+            .banner {
+                padding: 150px 20px 50px;
+                flex-direction: column-reverse;
+            }
+
+            .banner .image {
+                margin-left: 0;
+                max-width: 80%;
+                margin-bottom: 30px;
+            }
+
+            .banner .content h2 {
+                font-size: 2em;
+            }
+        }
+
+        @media(max-width:600px) {
+            .banner .image {
+                margin-top: 30px;
+                margin-bottom: 30px;
+            }
+        }
+    </style>
 </head>
+
 <body>
-    <?php include "../../resources/partials/header--default.html"?>
+    <?php include "../../resources/partials/header--default.html" ?>
+    <div class="banner">
+        <div class="content">
+            <h2>Sistema de reservaciones</h2>
+            <p>Actualmente en la IUSH existe un sistema interno para gestionar las reservas de
+                espacios, la cual no cuenta con una interacción con el usuario, además de no ser
+                viable para reservas de personal externo a la IUSH. Para esto se propone Aplicativo
+                web con diseño responsive para todo tipo de dispositivos, con diferentes perfiles de
+                usuario que permita hacer el registro de solicitudes con la finalidad de ser gestionado
+                por un administrador, con registro de solicitudes que permita graficar el uso de los
+                espacios y equipos tecnológicos de la IUSH. </p>
+            <a href="IUSH/">Ir a la IUSH</a>
+        </div>
+        <img src="/resources/assets/IUSH.png" class="image">
+    </div>
 </body>
-</html>
+
 </html>
--- public/services/equipment/index.php
+++ public/services/equipment/index.php
@@ -17,31 +17,31 @@
                     
                     <div class="col-md-4 d-flex justify-content-center align-items-center mb-5">
                         <div class="card" style="width: 18rem;">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
+                            <img src="https://www.ktronix.com/medias/194721307385-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wyODU0MDN8aW1hZ2UvcG5nfGltYWdlcy9oNDYvaDhkLzkzNTAwNzY3MjczMjYucG5nfDQxNjA5ZWQ3YzAxOTA5ZDg5ODNhN2FkNjg2MDMwMjJlZjZmNjM5ZGJjYjQzNzBmNDlkNTY1YzE4MjcyODlmMTY" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
+                                <h5 class="card-title">Portatiles</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary">Reservar</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4 d-flex justify-content-center align-items-center mb-5">
                         <div class="card" style="width: 18rem;">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
+                            <img src="https://www.mediatekis.com.co/media/catalog/product/cache/66f218621d96f987320b231940644298/A/T/ATR1100X_2.png" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
+                                <h5 class="card-title">Microfonos</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary">Reservar</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-4 d-flex justify-content-center align-items-center mb-5">
                         <div class="card" style="width: 18rem;">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
+                            <img src="https://mediaserver.goepson.com/ImConvServlet/imconv/5a5347ac791116534200f418cad7268599957d38/1200Wx1200H?use=banner&assetDescr=pls39_hero-left" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
+                                <h5 class="card-title">Proyectores</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary">Reservar</a>
                             </div>
                         </div>
                     </div>
@@ -51,21 +51,21 @@
                 <div class="row">
                     <div class="col-md-6 d-flex justify-content-center align-items-center mb-5">
                         <div class="card" style="width: 18rem;">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
+                            <img src="https://articaaudiovisual.com/wp-content/uploads/2017/01/sonido.png" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
+                                <h5 class="card-title">Equipo de Sonido</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary">Reservar</a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6 d-flex justify-content-center align-items-center mb-5">
                         <div class="card" style="width: 18rem;">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
+                            <img src="https://reset.net.co/wp-content/uploads/2020/01/Cable_HDMI_1.5m_Sencillo.webp" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
+                                <h5 class="card-title">Complementos</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary">Reservar</a>
                             </div>
                         </div>
                     </div>
