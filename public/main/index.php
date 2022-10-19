4b48bd286c9f77c3cd54158bf6d4f5eb93d9f6de
Sebastián López Osorno <100486485+SebastianLopezOsorno-SENA@users.noreply.github.com>
2022-10-19 14:21:00 -0500

    Update index.php

--- public/main/index.php
+++ public/main/index.php
@@ -42,7 +42,7 @@ include '../../server/security/seguridad.php';
                     </div>
                     <div class="col-md-6 d-flex justify-content-center align-items-center mb-5">
                         <div class="card" style="width: 18rem; background-color: #202020;">
-                            <img src="https://www.iush.edu.co/uploads/header/admisiones/calendario-academico.jpg" class="card-img-top" alt="...">
+                            <img src="https://th.bing.com/th/id/R.869488e3b02172604372560ad83a5618?rik=FyWq6nQXMMdLMA&pid=ImgRaw&r=0" class="card-img-top" alt="...">
                             <div class="card-body">
                                 <h5 class="card-title">Espacios</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
