59e8bcd0cdcc189f55ac0cdf287ed367418a9c40
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2022-11-10 13:34:25 -0500

    main

---
+++ public/loans/index.php
@@ -0,0 +1,41 @@
+<?php
+include '../../server/connection/conexion.php';
+include '../../server/security/seguridad.php';
+?>
+<?php if (!empty($user)) : ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <?php include "../resources/assets/meta.html" ?>
+
+    </head>
+
+    <body>
+        <?php include "../resources/partials/header--main.html" ?>
+        <div class="container-fluid text-light" style="background-color: #0D0D0D;">
+            
+        </div>
+        <?php include "../resources/partials/footer--default.html" ?>
+    </body>
+
+    </html>
+<?php else : ?>
+
+    <!DOCTYPE html>
+    <!--html sino ha iniciado sesion-->
+    <html>
+
+    <body>
+        <script>
+            setTimeout(alertFunc, 1000);
+
+            function alertFunc() {
+                location.replace("../login");
+            }
+        </script>
+    </body>
+
+    </html>
+
+<?php endif; ?>
