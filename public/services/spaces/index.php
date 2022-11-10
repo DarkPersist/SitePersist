dbe905d66bc4431ed80abcd6bb45424d7c7d5517
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2022-11-10 12:20:58 -0500

    Update index.php

--- public/services/spaces/index.php
+++ public/services/spaces/index.php
@@ -16,11 +16,21 @@
                 <div class="row">
                     <div class="col-md-4 d-flex justify-content-center align-items-center mb-5">
                         <div class="card" style="width: 18rem; background-color: #202020;">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
+                            <img src="https://www.iush.edu.co/uploads/20120612100220.jpg" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <h5 class="card-title">Bloque Tecnológico</h5>
+                                <p class="card-text">Este bloque cuenta con diversos espacios como lo son el auditorio, el lugar principal, además de diferentes salas de cómputos donde se pueden desarrollar variedad de clases enlazadas al mundo tecnológico.</p>
+                                <a href="#" class="btn btn-primary">Ir allá</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 d-flex justify-content-center align-items-center mb-5">
+                        <div class="card" style="width: 18rem; background-color: #202020">
+                            <img src="https://www.iush.edu.co/uploads/header/escuelas.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Bloque Academico</h5>
+                                <p class="card-text">Este bloque cuenta con diversos espacios académicos, donde se desarrollan la mayoría de las clases de la institución debido a que es el espacio perfecto para enseñar.</p>
+                                <a href="#" class="btn btn-primary">Ir allá</a>
                             </div>
                         </div>
                     </div>
@@ -30,41 +40,7 @@
                             <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4 d-flex justify-content-center align-items-center mb-5">
-                        <div class="card" style="width: 18rem; background-color: #202020">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="mb-0">
-                <div class="row">
-                    <div class="col-md-6 d-flex justify-content-center align-items-center mb-5">
-                        <div class="card" style="width: 18rem; background-color: #202020">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 d-flex justify-content-center align-items-center mb-5">
-                        <div class="card" style="width: 18rem; background-color: #202020">
-                            <img src="http://es.pecc-mexico.org/wp-content/uploads/2020/11/d437da2a54918895cf59a3362a0099ec-album-de-fotos-plano-icono-by-vexels.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <a href="#" class="btn btn-primary">Ir allá</a>
                             </div>
                         </div>
                     </div>
@@ -73,4 +49,5 @@
         </div>
     </div>
     <?php include "../../resources/partials/footer--default.html" ?>
+
 </html>
